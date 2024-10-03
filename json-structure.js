f07259a0e9a8a9324beebd9b3033b2fde20e2f9d
Rithika-05 <rithikavenkatesh99@gmail.com>
2024-10-03 23:21:33 +0530

    added data-types.js,execution-context.js,control-statements.js,hoisting.js,json-structure.js,value-and-reference-types.js,functions-and-closures.js

---
+++ json-structure.js
@@ -0,0 +1,15 @@
+var jasonStructure = [
+    { 
+      name: 'Rithika',
+      age: 25,
+      city: 'Chennai',
+      hobbies: ['Reading', 'Coding', 'Gaming']
+    },
+    { 
+      name: 'venkatesh',
+      age: 28,
+      city: 'Visakhapatnam',
+      hobbies: ['Traveling', 'Painting', 'Music']
+    }
+  ];
+console.log(jsonStructure);
